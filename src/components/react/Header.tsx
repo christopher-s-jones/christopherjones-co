7eef8962b9734425fd1cc12116efb6e0cf0878d6
Christopher Jones <152762801+christopher-s-jones@users.noreply.github.com>
2024-10-18 16:34:48 -0600

    Remove the logo and theme toggle temporarily:
    It seems like the ClientRouter is causing event
    propogation issues with the theme toggle, so disable
    it for now, but put it back in shortly when the
    issues are resolved.

--- src/components/react/Header.tsx
+++ src/components/react/Header.tsx
@@ -1,7 +1,7 @@
 import Logo from "./Logo"
 import MobileNav from "./MobileNav";
 import DesktopNav from "./DesktopNav";
-import ThemeToggle from "./ThemeToggle";
+// import ThemeToggle from "./ThemeToggle";
 import { useStore } from "@nanostores/react";
 import { $isMobile } from "../../stores/mobileState";
 
@@ -17,23 +17,26 @@ export default function Header() {
                 top-0 left-0 right-0 z-50
                 backdrop-blur-md
                 bg-white
+                px-6 py-0 md:py-2
                 bg-opacity-95
                 dark:bg-opacity-50
                 dark:bg-slate-950"
         >
             <div
                 className="flex items-center justify-between min-h-20
-                    max-w-7xl 3xl:max-w-[2560px] mx-auto px-6 py-0 sm:py-1 md:py-6"
+                    max-w-7xl 3xl:max-w-[2560px] mx-auto sm:py-1"
             >
-                <Logo />
+                {/* <Logo /> */}
                 <div className="content" />
                 <div className="flex flex-end flex-row-reverse">
                     {/**ThemeToggle is rendering in the opposite order,
                      * so reverse the order here for both mobile and desktop.
                      * Odd behavior =)
                      */}
-                    {isMobile && (<><MobileNav /><ThemeToggle /></>)}
-                    {!isMobile && (<><ThemeToggle /><DesktopNav /></>)}
+                    {/* {isMobile && (<><MobileNav /><ThemeToggle /></>)}
+                    {!isMobile && (<><ThemeToggle /><DesktopNav /></>)} */}
+                    {isMobile && (<><MobileNav /></>)}
+                    {!isMobile && (<><DesktopNav /></>)}
                 </div>
             </div>
         </header>
