3509f8a1c4fd85b44227610271da76bfe654c251
Christopher Jones <152762801+christopher-s-jones@users.noreply.github.com>
2026-01-23 11:07:45 -0500

    Add a CNAME file and the site property to Astro.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  site: 'https://christopherjones.co',
   integrations: [react()]
 });
